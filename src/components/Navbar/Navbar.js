e9273a1623c9e0b3b375027c20407dbf79e07387
Shahtaz Rahman <shahtazrahman17@gmail.com>
2022-11-08 15:03:57 +0600

    navbar added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,40 @@
-import React from 'react'
-import './Navbar.css';
+import React, { useState } from 'react'
+import './Navbar.css'
+import logo from '../../assets/icons/logo.png';
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faXmark} from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false)
   return (
-    <div>Navbar</div>
+    <nav>
+      <div className='nav-container'>
+
+        <div className='toggle-icon' onClick={() => setOpen(!open)}>
+          {open ? <FontAwesomeIcon icon={faXmark} /> : <FontAwesomeIcon icon={faBars} />}
+        </div>
+
+        <div className='nav-logo'>
+          <Link to='/'>
+            <img src={logo} alt="ada academy logo"></img>
+            <h2>adornment</h2>
+          </Link>
+        </div>
+
+        <div className={`nav-items ${open ? 'shown' : 'hide'}`}>
+          <Link to='/'>Home</Link>
+          <Link to='/courses'>Services</Link>
+          <Link to='/faq'>FAQ</Link>
+          <Link to='/blogs'>Blogs</Link>
+        </div>
+
+        <div className='nav-items2'>
+          <Link to='/login'>Sign In</Link>
+          {/* <Link to='/sign-up'>Sign Up</Link> */}
+        </div>
+      </div>
+    </nav>
   )
 }
 
