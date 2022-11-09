d64ba262bf52ac03f0efb6bdfe1e508fbc1724da
Shahtaz Rahman <shahtazrahman17@gmail.com>
2022-11-10 03:45:23 +0600

    context-api added

---
+++ src/context/AuthProvider.js
@@ -0,0 +1,20 @@
+import React, { createContext } from 'react'
+import app from '../firebase/firebase.config'
+import { getAuth } from 'firebase/auth'
+
+export const AuthContext = createContext()
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+    const user = {displaName: 'shahtaz'}
+    const authInfo = {
+        user
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthProvider
